05c69edbf99c5b74e959e5d3e16e52fde4e4b6c7
Igors78 <oleinikovsigors@gmail.com>
2021-10-18 11:00:15 +0300

    Fix expanding inside double quotes and stick string back together

--- util.c
+++ util.c
@@ -3,17 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   util.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*   By: ioleinik <ioleinik@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 12:50:54 by ioleinik          #+#    #+#             */
-/*   Updated: 2021/10/17 17:15:09 by mbarut           ###   ########.fr       */
+/*   Updated: 2021/10/18 10:25:47 by ioleinik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	chk_sing_quo(t_data *d, char **spl, int *i, char *tmp)
+void	chk_sing_quo(t_data *d, char **spl, int *i)
 {
+	char	*tmp;
+
 	if (spl[*i][0] == '\'' && spl[*i][1] == '$'
 			&& spl[*i][ft_strlen(spl[*i]) - 1] == '\'')
 	{
@@ -24,17 +26,21 @@ int	chk_sing_quo(t_data *d, char **spl, int *i, char *tmp)
 			tmp = ft_getenv(d, &spl[*i][3]);
 		}
 		else
+		{
+			spl[*i][ft_strlen(spl[*i]) - 1] = '\0';
 			tmp = ft_getenv(d, &spl[*i][2]);
+		}
 		if (!tmp)
-			return (1);
+			return ;
 		free(spl[*i]);
 		spl[*i] = ft_strdup(tmp);
 	}
-	return (0);
 }
 
-int	chk_curl_br(t_data *d, char **spl, int *i, char *tmp)
+void	chk_curl_br(t_data *d, char **spl, int *i)
 {
+	char	*tmp;
+
 	if (spl[*i][0] == '$')
 	{
 		if (spl[*i][1] == '{'
@@ -46,11 +52,10 @@ int	chk_curl_br(t_data *d, char **spl, int *i, char *tmp)
 		else
 			tmp = ft_getenv(d, &spl[*i][1]);
 		if (!tmp)
-			return (1);
+			return ;
 		free(spl[*i]);
 		spl[*i] = ft_strdup(tmp);
 	}
-	return (0);
 }
 
 void	free_memory(t_data *d)
@@ -63,4 +68,15 @@ void	free_memory(t_data *d)
 		free(d->line);
 	if (d->path)
 		free(d->path);
+}
+
+void	put_strarr(char **arr)
+{
+	int	i = 0;
+
+	while (arr[i])
+	{
+		printf("%s\n", arr[i]);
+		i++;
+	}
 }
