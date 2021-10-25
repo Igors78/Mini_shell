27224914b21533390d27eccad5430942623f5f13
mbarut <mbarut@student.42wolfsburg.de>
2021-10-25 17:06:24 +0200

    fixed not exiting out of a process in command not found errors

--- handle_exec.c
+++ handle_exec.c
@@ -6,11 +6,17 @@
 /*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 12:49:14 by mbarut            #+#    #+#             */
-/*   Updated: 2021/10/24 12:50:27 by mbarut           ###   ########.fr       */
+/*   Updated: 2021/10/25 16:58:12 by mbarut           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	exit_gracefully(char **args)
+{
+	ft_split_free(args);
+	exit(EXIT_FAILURE);
+}
 
 void	handle_exec(t_data *d, int i)
 {
@@ -24,27 +30,21 @@ void	handle_exec(t_data *d, int i)
 	//int k = 0;
 	//while (args[k])
 	//{
-	//	printf("[debug] args[%d][%d]: %s\n", i, k, args[k]);
+	//	printf("[debug] args[%d][%d]: %s$\n", i, k, args[k]);
 	//	k++;
 	//}
 	/* /debug */
 	if (ft_strchr(args[0], '/'))
 	{
 		if (execve(args[0], args, d->envv) == -1)
-		{
-			perror(args[0]);
-			ft_split_free(args);
-			exit(EXIT_FAILURE);
-		}
+			exit_gracefully(args);
 	}
 	else if (is_builtin(args[0]))
 		handle_builtins(d, args);
 	else
 	{
 		check_path(d, args[0]);
-		if (!d->path)
-			return ;
-		if (execve(d->path, args, d->envv) == -1)
-			perror("execve() failed");
+		if (!d->path || execve(d->path, args, d->envv) == -1)
+			exit_gracefully(args);
 	}
 }
